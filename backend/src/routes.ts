f39ab039555f43b8bd1262d8778b5fc2f4113265
Giovani Moutinho <giovanim@ymail.com>
2020-06-06 15:52:31 -0300

    Configura arquivos estáticos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import path from 'path';
 
 const routes = express.Router();
 
 routes.get('/', (request, response) => {
   return response.json({ msg: 'Hello World' });
 });
+
+routes.use(
+  '/uploads',
+  express.static(path.resolve(__dirname, '..', 'uploads')),
+);
 
 export default routes;
