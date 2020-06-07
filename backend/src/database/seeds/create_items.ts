f8cad3027494dc8c7b3bf10ed90d3999d820fc27
Giovani Moutinho <giovanim@ymail.com>
2020-06-07 17:15:43 -0300

    Corrige url de uploads

--- backend/src/database/seeds/create_items.ts
+++ backend/src/database/seeds/create_items.ts
@@ -2,7 +2,7 @@ import Knex from 'knex';
 
 export async function seed(knex: Knex) {
   await knex('items').insert([
-    { title: 'Lâmpadas', image: 'lampas.svg' },
+    { title: 'Lâmpadas', image: 'lampadas.svg' },
     { title: 'Pilhas e Baterias', image: 'baterias.svg' },
     { title: 'Papéis e Papelão', image: 'papelao.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
