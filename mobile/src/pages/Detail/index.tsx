b0907ffceba4afeb6a2662ac4e85faba99a9de93
Giovani Moutinho <giovanim@ymail.com>
2020-06-14 11:46:33 -0300

    Altera url de exibição de imagem no mobile

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -23,6 +23,7 @@ interface Data {
   point: {
     name: string;
     image: string;
+    image_url: string;
     email: string;
     whatsapp: string;
     city: string;
@@ -77,7 +78,7 @@ const Detail = () => {
         <Image
           style={styles.pointImage}
           source={{
-            uri: data.point.image,
+            uri: data.point.image_url,
           }}
         />
         <Text style={styles.pointName}>{data.point.name}</Text>
